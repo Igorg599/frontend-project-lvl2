43dfabcad567ba18739933a44e5a775d3be0f98d
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-02-21 20:41:56 +0500

    add genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { Command } from "commander"
+import genDiff from "../src/index.js"
 
 const program = new Command()
 
 program
   .version("0.0.1", "-V, --version", "output the version number")
   .description("Compares two configuration files and shows a difference.")
-  .argument("<filepath1> <filepath2>")
+  .arguments("<filepath1> <filepath2>")
   .option("-f, --format [type]", "output format")
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2))
+  })
 program.parse()
