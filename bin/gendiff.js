a6c583970ae74aabc48a922f4eb3c810869977a1
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-02-21 18:35:33 +0500

    edit options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,5 @@ program
   .version("0.0.1", "-V, --version", "output the version number")
   .description("Compares two configuration files and shows a difference.")
   .argument("<filepath1> <filepath2>")
-
+  .option("-f, --format [type]", "output format")
 program.parse()
