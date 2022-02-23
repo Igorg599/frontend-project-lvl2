137de0ea77e54a7286838ae243566abc2d3d4fc5
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-02-23 13:21:23 +0500

    settings eslint and make

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-import _ from "lodash"
-import { readFileSync } from "fs"
-import path from "path"
+import _ from 'lodash';
+import { readFileSync } from 'fs';
+import path from 'path';
 
-const fileParse = (file) => JSON.parse(readFileSync(path.resolve(file), "utf8"))
+const fileParse = (file) => JSON.parse(readFileSync(path.resolve(file), 'utf8'));
 
-const operators = ["+", "-"]
+const operators = ['+', '-'];
 
 const genDiff = (file1, file2) => {
-  const fileJson1 = fileParse(file1)
-  const fileJson2 = fileParse(file2)
-  let result = {}
+  const fileJson1 = fileParse(file1);
+  const fileJson2 = fileParse(file2);
+  let result = {};
 
-  const keys1 = Object.keys(fileJson1)
-  const keys2 = Object.keys(fileJson2)
-  const keysAll = _.union(keys1, keys2)
-  keysAll.sort()
+  const keys1 = Object.keys(fileJson1);
+  const keys2 = Object.keys(fileJson2);
+  const keysAll = _.union(keys1, keys2);
+  keysAll.sort();
 
-  for (const key of keysAll) {
+  for (let key = 1; key <= keysAll.length; key += 1) {
     if (
-      _.has(fileJson1, key) &&
-      _.has(fileJson2, key) &&
-      fileJson1[key] === fileJson2[key]
+      _.has(fileJson1, key)
+      && _.has(fileJson2, key)
+      && fileJson1[key] === fileJson2[key]
     ) {
-      result[`  ${key}`] = fileJson1[key]
+      result[`  ${key}`] = fileJson1[key];
     } else if (
-      _.has(fileJson1, key) &&
-      _.has(fileJson2, key) &&
-      fileJson1[key] !== fileJson2[key]
+      _.has(fileJson1, key)
+      && _.has(fileJson2, key)
+      && fileJson1[key] !== fileJson2[key]
     ) {
-      result[`${operators[1]} ${key}`] = fileJson1[key]
-      result[`${operators[0]} ${key}`] = fileJson2[key]
+      result[`${operators[1]} ${key}`] = fileJson1[key];
+      result[`${operators[0]} ${key}`] = fileJson2[key];
     } else if (!_.has(fileJson1, key) || _.has(fileJson2, key)) {
-      result[`${operators[0]} ${key}`] = fileJson2[key]
+      result[`${operators[0]} ${key}`] = fileJson2[key];
     } else if (_.has(fileJson1, key) || !_.has(fileJson2, key)) {
-      result[`${operators[1]} ${key}`] = fileJson1[key]
+      result[`${operators[1]} ${key}`] = fileJson1[key];
     }
   }
-  result = JSON.stringify(result, null, 2)
-  return result.replace(/"/g, "")
-}
+  result = JSON.stringify(result, null, 2);
+  return result.replace(/"/g, '');
+};
 
-export default genDiff
+export default genDiff;
