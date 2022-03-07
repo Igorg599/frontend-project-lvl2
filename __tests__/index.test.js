92b62890a32dfd61ac1d6887da4e53fad65ddd13
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-03-07 18:22:07 +0500

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,17 +3,16 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('check for correct diff', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(
-    readFile('expectFiles.json'),
-  );
-  expect(genDiff('file1.json', 'file2.json')).toEqual(
+  // expect(genDiff("__fixtures__/file1.json", "__fixtures__/file2.json")).toEqual(
+  //   readFile("expectFiles.json")
+  // )
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(
     readFile('expectFiles.txt'),
   );
 });
