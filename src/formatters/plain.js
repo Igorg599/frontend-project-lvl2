0de7d458b154c0aea4e9008cd264dd829902f98c
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-03-15 09:27:16 +0500

    fix plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,34 +1,40 @@
 import _ from 'lodash';
 
-const isObject = (val) => {
-  if (_.isPlainObject(val)) return '[complex value]';
-  return typeof val === 'string' ? `'${val}'` : val;
+const stringify = (val) => {
+  if (_.isPlainObject(val) && val !== null) return '[complex value]';
+  if (typeof val === 'string') {
+    return `'${val}'`;
+  }
+  if (val === null) {
+    return null;
+  }
+  return `${val}`;
 };
 
 const plain = (data) => {
   const iter = (tree, parent) => tree
-    .filter((node) => node[0] !== 'same')
+    .filter((node) => node.type !== 'same')
     .map((node) => {
-      const property = parent ? `${parent}.${node[1].key}` : node[1].key;
-      if (node[1].val === null) {
-        return null;
+      const property = parent ? `${parent}.${node.key}` : node.key;
+      switch (node.type) {
+        case 'add':
+          return `Property '${property}' was added with value: ${stringify(
+            node.val,
+          )}`;
+        case 'remove':
+          return `Property '${property}' was removed`;
+        case 'updated':
+          return `Property '${property}' was updated. From ${stringify(
+            node.val1,
+          )} to ${stringify(node.val2)}`;
+        case 'recursion':
+          return `${iter(node.children, property)}`;
+        default:
+          throw new Error(`Type is not defined - ${node.type}`);
       }
-      if (node[0] === 'add') {
-        return `Property '${property}' was added with value: ${isObject(
-          node[1].val,
-        )}`;
-      }
-      if (node[0] === 'remove') {
-        return `Property '${property}' was removed`;
-      }
-      if (node[0] === 'updated') {
-        return `Property '${property}' was updated. From ${isObject(
-          node[1].val1,
-        )} to ${isObject(node[1].val2)}`;
-      }
-      return `${iter(node[1].val, property).join('\n')}`;
-    });
-  return `${iter(data, 0).join('\n')}`;
+    })
+    .join('\n');
+  return iter(data, 0);
 };
 
 export default plain;
